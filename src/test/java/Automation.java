d291aa18520961de86a584889f48131a540faf64
RonielsonOliveira <ronielsonoliveira@live.com>
2023-07-12 23:02:51 -0300

    first commit

--- src/test/java/Automation.java
+++ src/test/java/Automation.java
@@ -1,21 +1,15 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
-import java.awt.dnd.DropTarget;
 import java.time.Duration;
 
-public class Automacao extends TestBase {
+public class Automation extends TestBase {
 
 // Cenario 1
   @Test
